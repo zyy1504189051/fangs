4aeaf55327b91a20f4136f4759a258e613e74422
zyy1504189051 <zhoubiao150418@163.com>
2019-06-19 18:03:20 +0800

    fang_work

---
+++ fang/ihome/constants.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+
+# 图片验证码的redis有效期, 单位：秒
+IMAGE_CODE_REDIS_EXPIRES = 180
+
+# 短信验证码的redis有效期, 单位：秒
+SMS_CODE_REDIS_EXPIRES = 300
+
+# 发送短信验证码的间隔, 单位：秒
+SEND_SMS_CODE_INTERVAL = 60
+
+# 登录错误尝试次数
+LOGIN_ERROR_MAX_TIMES = 5
+
+# 登录错误限制的时间, 单位：秒
+LOGIN_ERROR_FORBID_TIME = 600
+
+# 七牛的域名
+QINIU_URL_DOMAIN = "http://prxtg8d0l.bkt.clouddn.com/"
+
+# 城区信息的缓存时间, 单位：秒
+AREA_INFO_REDIS_CACHE_EXPIRES = 7200
---
+++ fang/logs/01.py
@@ -0,0 +1,13 @@
+# # print("fsfsgfs\ngs")
+# # print('fwsegfs\nergsgs')
+# print('"dafafa"')
+# print("'fdafa-'")
+b='fed' \
+  'fs'
+a="fsd" \
+  "fs"
+str1 = '''aaa
+bbb'''
+print(str1)
+print(a)
+print(b)
